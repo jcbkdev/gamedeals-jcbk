b62459a1719f7a19607efdc101d47ae4c81fb771
jcbkdev <kubkon134@gmail.com>
2024-12-07 21:29:06 +0100

    deals section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Hero from "./sections/hero/hero";
+import Deals from "./sections/deals/deals";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Deals />
     </>
   );
 }
---
+++ src/sections/deals/deals.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+import GameCard from "../../components/gameCard/gameCard";
+
+export default function Deals() {
+  return (
+    <section className="deals">
+      <h2 className="deals-title">Currently Available Free Games</h2>
+      <div className="deals-container"></div>
+    </section>
+  );
+}
