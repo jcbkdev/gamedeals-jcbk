dd9b7b1d33b65caaa41be63e3416d6ff8568f632
jcbkdev <kubkon134@gmail.com>
2025-12-06 19:11:41 +0100

    [ghpages] fixed fetching

--- src/app/sections/Deals/Deals.tsx
+++ src/app/sections/Deals/Deals.tsx
@@ -1,35 +1,59 @@
+"use client";
+
 import styles from "./styles.module.css";
 import DealCard from "@/app/components/DealCard/DealCard";
 import DealDetails from "@/app/components/DealDetails/DealDetails";
 import { Game } from "@/types/game.type";
-import { Suspense } from "react";
+import { useEffect, useState } from "react";
 
-export default async function Deals() {
-  const deals: Game[] | null = await fetch(
-    process.env.NEXT_PUBLIC_DEALS_URI!
-  ).then((res) => {
-    if (res.status === 200) return res.json();
-    else return null;
-  });
+export default function Deals() {
+  const [deals, setDeals] = useState<Game[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchDeals = async () => {
+      try {
+        const res = await fetch(process.env.NEXT_PUBLIC_DEALS_URI!).then(
+          (res) => {
+            if (res.status === 200) return res.json();
+            else return null;
+          }
+        );
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setDeals(data);
+        } else {
+          setError(true);
+        }
+      } catch (err) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDeals();
+  }, []);
+
+  if (loading) return <p>Loading deals...</p>;
+  if (error) return <p>No deals could get fetched</p>;
 
   return (
     <section className={styles.dealsSection}>
       <h2>Currently Available Free Games</h2>
       <div className={styles.dealsContainer}>
-        {deals ? (
-          deals.length >= 1 ? (
-            deals.map((d) => (
-              <Suspense key={d.id} fallback={<DealCard deal={d} />}>
-                <DealDetails deal={d}>
-                  <DealCard deal={d} />
-                </DealDetails>
-              </Suspense>
-            ))
-          ) : (
-            <p>No deals available</p>
-          )
+        {deals && deals.length >= 1 ? (
+          deals.map((d) => (
+            <div key={d.id}>
+              <DealDetails deal={d}>
+                <DealCard deal={d} />
+              </DealDetails>
+            </div>
+          ))
         ) : (
-          <p>No deals could get fetched</p>
+          <p>No deals available</p>
         )}
       </div>
     </section>
