df81890955e1547d7c7a10d4b92587c826b7429f
jcbkdev <kubkon134@gmail.com>
2026-01-07 22:53:47 +0100

    [fix] replace igdb image with gamerpower's

--- src/app/components/DealDetails/DealDetails.tsx
+++ src/app/components/DealDetails/DealDetails.tsx
@@ -7,6 +7,7 @@ import { useSearchParams } from "next/navigation";
 import { ReactNode } from "react";
 import Button from "../Button/Button";
 import Tag from "../Tag/Tag";
+import { isUrl } from "@/utils/isurl";
 
 type Props = {
   deal: Game;
@@ -20,6 +21,10 @@ export default function DealDetails(props: Props) {
   const isOpen = dealId === props.deal.id.toString();
 
   const coverImage64 = props.deal.images[1] ?? props.deal.images[0];
+
+  let imageUrl = isUrl(coverImage64)
+    ? coverImage64
+    : `data:image/jpeg;base64,${coverImage64}`;
 
   const handleOpen = () => {
     history.replaceState(null, "", `/?deal=${props.deal.id}`);
@@ -38,19 +43,21 @@ export default function DealDetails(props: Props) {
     >
       <div className={styles.dealDetails}>
         <div className={styles.cover}>
-          <img src={`data:image/jpeg;base64,${coverImage64}`} alt="" />
+          <img src={imageUrl} alt="" />
         </div>
         <div className={styles.details}>
           <h1 className={styles.title}>{props.deal.name}</h1>
           <h2 className={styles.description}>{props.deal.description}</h2>
-          <div>
-            <h3 className={styles.tagsTitle}>Tags:</h3>
-            <div className={styles.tags}>
-              {props.deal.tags.map((t, index) => (
-                <Tag key={index}>{t}</Tag>
-              ))}
+          {props.deal.tags.length > 0 && (
+            <div>
+              <h3 className={styles.tagsTitle}>Tags:</h3>
+              <div className={styles.tags}>
+                {props.deal.tags.map((t, index) => (
+                  <Tag key={index}>{t}</Tag>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
           <a
             className={styles.button}
             href={props.deal.url}
