70967d6b835824f100520e81275bc12b90f8d5e1
jcbkdev <kubkon134@gmail.com>
2026-02-07 21:54:04 +0100

    update footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,11 +3,15 @@ import styles from "./style.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <span>
-        © 2025{" "}
-        <a href="https://jcbk.pl" rel="noopener noreferrer" target="_blank">
-          JCBK
-        </a>
+      <span className={styles.disclaimer}>
+        <h3>Disclaimer</h3>
+        <p>
+          Powered by Steam, a registered trademark of Valve Corporation. This
+          website is in no way affiliated with or endorsed by Valve Corporation.
+          We cannot guarantee the accuracy or availability of the displayed
+          offers - before you buy something in a store, make sure that the
+          prices are correct.{" "}
+        </p>
       </span>
       <span>
         Powered by{" "}
@@ -21,7 +25,23 @@ export default function Footer() {
         &{" "}
         <a href="https://igdb.com" rel="noopener noreferrer" target="_blank">
           IGDB
+        </a>{" "}
+        &{" "}
+        <a
+          href="https://store.steampowered.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Steam
         </a>
+      </span>
+      <span>
+        © 2026{" "}
+        <a href="https://jcbk.pl" rel="noopener noreferrer" target="_blank">
+          JCBK
+        </a>
+        . All rights reserved. All trademarks are the property of their
+        respective owners.
       </span>
     </footer>
   );
