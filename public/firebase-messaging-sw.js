006e91c152562aa837d992f66e57a574c0b76600
jcbkdev <kubkon134@gmail.com>
2026-03-06 20:39:57 +0100

    [fix] doubled notifications

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -17,13 +17,3 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/logo.png",
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
